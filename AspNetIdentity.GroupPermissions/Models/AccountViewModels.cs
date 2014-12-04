15e42206f0a6f180ef292d3a3bf36dba6d380c54
wzorekmj <wzorekmj@gmail.com>
2014-12-04 17:53:46 -0500

    test

--- AspNetIdentity.GroupPermissions/Models/AccountViewModels.cs
+++ AspNetIdentity.GroupPermissions/Models/AccountViewModels.cs
@@ -7,7 +7,7 @@ namespace AspNetIdentity.GroupBasedPermissions.Models
     {
         [Required]
         [Display(Name = "Email")]
-        public string Email { get; set; }
+        public string Email { get; set; } 
     }
 
     public class ExternalLoginListViewModel
