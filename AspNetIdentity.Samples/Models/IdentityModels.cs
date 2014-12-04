e82de43b8d4121245f261ff08f9120951a92365c
wzorekmj <wzorekmj@gmail.com>
2014-12-04 17:39:21 -0500

    First commit

---
+++ AspNetIdentity.Samples/Models/IdentityModels.cs
@@ -0,0 +1,125 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System;
+
+namespace AspNetIdentity.GroupBasedPermissions.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        // Add the ApplicationGroups property:
+        public virtual IDbSet<ApplicationGroup> ApplicationGroups { get; set; }
+
+        // Override OnModelsCreating:
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Make sure to call the base method first:
+            base.OnModelCreating(modelBuilder);
+
+
+            // Map Users to Groups:
+            modelBuilder.Entity<ApplicationGroup>()
+                .HasMany<ApplicationUserGroup>((ApplicationGroup g) => g.ApplicationUsers)
+                .WithRequired()
+                .HasForeignKey<string>((ApplicationUserGroup ag) => ag.ApplicationGroupId);
+            modelBuilder.Entity<ApplicationUserGroup>()
+                .HasKey((ApplicationUserGroup r) =>
+                    new
+                    {
+                        ApplicationUserId = r.ApplicationUserId,
+                        ApplicationGroupId = r.ApplicationGroupId
+                    }).ToTable("ApplicationUserGroups");
+
+            // Map Roles to Groups:
+            modelBuilder.Entity<ApplicationGroup>()
+                .HasMany<ApplicationGroupRole>((ApplicationGroup g) => g.ApplicationRoles)
+                .WithRequired()
+                .HasForeignKey<string>((ApplicationGroupRole ap) => ap.ApplicationGroupId);
+            modelBuilder.Entity<ApplicationGroupRole>().HasKey((ApplicationGroupRole gr) =>
+                new
+                {
+                    ApplicationRoleId = gr.ApplicationRoleId,
+                    ApplicationGroupId = gr.ApplicationGroupId
+                }).ToTable("ApplicationGroupRoles");
+        }
+    }
+
+    public class ApplicationGroup
+    {
+        public ApplicationGroup()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.ApplicationRoles = new List<ApplicationGroupRole>();
+            this.ApplicationUsers = new List<ApplicationUserGroup>();
+        }
+
+
+        public ApplicationGroup(string name)
+            : this()
+        {
+            this.Name = name;
+        }
+
+
+        public ApplicationGroup(string name, string description)
+            : this(name)
+        {
+            this.Description = description;
+        }
+
+
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<ApplicationGroupRole> ApplicationRoles { get; set; }
+        public virtual ICollection<ApplicationUserGroup> ApplicationUsers { get; set; }
+    }
+
+
+    public class ApplicationUserGroup
+    {
+        public string ApplicationUserId { get; set; }
+        public string ApplicationGroupId { get; set; }
+    }
+
+
+    public class ApplicationGroupRole
+    {
+        public string ApplicationGroupId { get; set; }
+        public string ApplicationRoleId { get; set; }
+    }
+}
